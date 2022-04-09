d04d383d5eff2caceb6afffcefaee35e54811920
Vinícius Plaza <vini_a.m.plaza@hotmail.com>
2022-04-09 09:17:08 -0300

    Ajustando mvvm

--- MasterChef/Automapper/ViewModelToDomain/ReceitaViewModelToDomain.cs
+++ MasterChef/Automapper/ViewModelToDomain/ReceitaViewModelToDomain.cs
@@ -11,22 +11,9 @@ namespace MasterChef.Automapper.ViewModelToDomain
         {
 
 
-            //CreateMap<CreateReceitaViewModel, ReceitaCreateRequest>().ReverseMap();
+            CreateMap<CreateReceitaViewModel, ReceitaCreateRequest>().ReverseMap();
             //CreateMap<CreateReceitaViewModel, Receita>().ReverseMap();
-            CreateMap<CreateReceitaViewModel, ReceitaCreateRequest>().
-                ConvertUsing((source, dest) =>
-                 {
-                     ReceitaCreateRequest result = dest ?? new ReceitaCreateRequest();
-                     if (source != null)
-                     {
-                         result.FotoName = System.IO.Path.GetFileName(source.FileName);
-                         result.FotoContent = source.ContentType;
-                         using (var reader = new System.IO.BinaryReader(source.InputStream))
-                             result.FotoContent = reader.ReadBytes(source.ContentLength);
-                     }
-                     return result;
-                 });
+
         }
-        
     }
 }
