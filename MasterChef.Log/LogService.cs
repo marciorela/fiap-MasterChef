10920fd57cc2e423661763cbd7b815df8643472d
Marcio Rela <marciorela@hotmail.com>
2022-04-11 17:18:40 -0300

    Projeto de Log

---
+++ MasterChef.Log/LogService.cs
@@ -0,0 +1,36 @@
+﻿using Serilog;
+using Microsoft.Extensions.Hosting;
+
+namespace MasterChef.Log
+{
+    public static class LogService
+    {
+        public static void Configure(IHostBuilder builderHost)
+        {
+            builderHost.UseSerilog((ctx, lc) => lc
+              .Enrich.WithMachineName()
+              .Enrich.WithEnvironmentUserName()
+              .Enrich.WithAssemblyVersion()
+              .Enrich.WithMemoryUsage()
+              .WriteTo.Async(w =>
+              {
+                  w.File(
+                      Path.Combine(AppDomain.CurrentDomain.BaseDirectory.ToString(), "logs", "log_.txt"),
+                      rollingInterval: RollingInterval.Day,
+                      fileSizeLimitBytes: 1024 * 1024 * 20,
+                      buffered: true,
+                      flushToDiskInterval: TimeSpan.FromSeconds(1),
+                      rollOnFileSizeLimit: true,
+                      outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u4}] | {AssemblyVersion} | {MemoryUsage} | {Message:l}{NewLine}{Exception}"
+                   );
+              }, bufferSize: 500)
+            );
+        }
+
+        public static void CloseAndFlush()
+        {
+            Serilog.Log.CloseAndFlush();
+        }
+
+    }
+}
