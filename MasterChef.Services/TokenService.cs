bf74169e700f1407f2fbe3449ffe32cc498a37c2
ALDO CESAR VISCOVICCE <RM342846@fiap.com.br>
2022-04-25 13:57:25 -0300

    Adicionando Memory Cache

--- MasterChef.Services/TokenService.cs
+++ MasterChef.Services/TokenService.cs
@@ -1,5 +1,6 @@
 ﻿using MasterChef.Contracts.Services;
 using MasterChef.Domain.Dto;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using RestSharp;
 
@@ -7,13 +8,21 @@ namespace MasterChef.Services
 {
     public class TokenService : ITokenService
     {
-        public TokenService()
+        private readonly IMemoryCache _memoryCache;
+        private const string TOKEN_KEY = "token";
+
+        public TokenService(IMemoryCache memoryCache)
         {
-                
+            _memoryCache = memoryCache;
         }
 
         public async Task<string> ObterTokenAsync(IConfiguration config, RestClient client)
-        {
+        {            
+            if (_memoryCache.TryGetValue(TOKEN_KEY, out AuthResponse authResponse))
+            {
+                return authResponse.Token;
+            }
+
             var request = new RestRequest("auth", Method.Post)
                 .AddJsonBody(new AuthRequest
                 {
@@ -23,7 +32,17 @@ namespace MasterChef.Services
             var response = await client.PostAsync<AuthResponse>(request);
 
             if (response != null)
-            {
+            {                
+                _memoryCache
+                    .Set(
+                    TOKEN_KEY,
+                    response,
+                    new MemoryCacheEntryOptions //Tempo para manter em memória
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(response.ExpiresIn), //Tempo de expiração apartir de agora
+                        SlidingExpiration = TimeSpan.FromSeconds(response.ExpiresIn / 2) //Caso não seja utilizado, vai remover nesse tempo
+                    });
+
                 return response.Token;
             }
 
